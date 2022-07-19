9c32705537c87f4de408185f1518c3d22805a2e2
jerbianis <anisjerbi2@gmail.com>
2022-07-19 17:25:41 +0100

    authorization for applying in invisible offer

--- app/Http/Requests/StoreCandidatureRequest.php
+++ app/Http/Requests/StoreCandidatureRequest.php
@@ -2,10 +2,31 @@
 
 namespace App\Http\Requests;
 
+use App\Models\JobOffer;
+use Carbon\Carbon;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreCandidatureRequest extends FormRequest
 {
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $job_offers=JobOffer::where('visibility',true)
+            ->where('offer_start_on','<=',Carbon::now())
+            ->where(function ($query) {
+                $query->where('offer_ends_on','>=',Carbon::now())
+                    ->orwhere('offer_ends_on',null);
+            })->get('id');
+        if (!$job_offers->contains($this->offer)) {
+            return false;
+        }else {
+            return true;
+        }
+    }
 
     /**
      * Get the validation rules that apply to the request.
